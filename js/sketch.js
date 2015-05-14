ea5864a782d8fab73128a24e91fa6029bc04c57d
Piyoros Vephula <epoch148@hotmail.com>
2015-05-13 20:32:41 -0700

    clean up code

--- js/sketch.js
+++ js/sketch.js
@@ -1,5 +1,5 @@
 //global vars
-// David
+
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 
@@ -11,14 +11,8 @@ window.onload = init;
 var context;
 var bufferLoader;
 var analyser;
-var source;
 var dataArray;
 var soundLoaded = false; 
-var dae;
-var manager;
-
-
-
 
 //****************************** music functions ******************************
 
@@ -35,7 +29,9 @@ function init() {
 	bufferLoader = new BufferLoader(
 	context,
 	[
-	'music/Moments.mp3',
+	'audio/Bloom.mp3',
+	'audio/Drowning.mp3',
+	'audio/Wildfire.mp3',
 	],
 	finishedLoading
 	);
@@ -69,13 +65,13 @@ function init() {
 }
 
 function finishedLoading(bufferList) {
-	var source1 = context.createBufferSource();
-	source1.buffer = bufferList[0];
+	var source = context.createBufferSource();
+	source.buffer = bufferList[Math.floor((Math.random() * 2))];
 
-	source1.connect(context.destination);
-	source1.connect(analyser);
+	source.connect(context.destination);
+	source.connect(analyser);
 
-	source1.start(0);
+	source.start(0);
 
 	soundLoaded = true;
 }
@@ -83,50 +79,50 @@ function finishedLoading(bufferList) {
 //********** BufferLoader Class **********
 
 function BufferLoader(context, urlList, callback) {
-  this.context = context;
-  this.urlList = urlList;
-  this.onload = callback;
-  this.bufferList = new Array();
-  this.loadCount = 0;
+	this.context = context;
+	this.urlList = urlList;
+	this.onload = callback;
+	this.bufferList = new Array();
+	this.loadCount = 0;
 }
 
 BufferLoader.prototype.loadBuffer = function(url, index) {
-  // Load buffer asynchronously
-  var request = new XMLHttpRequest();
-  request.open("GET", url, true);
-  request.responseType = "arraybuffer";
+	// Load buffer asynchronously
+	var request = new XMLHttpRequest();
+	request.open("GET", url, true);
+	request.responseType = "arraybuffer";
 
-  var loader = this;
+	var loader = this;
 
-  request.onload = function() {
-    // Asynchronously decode the audio file data in request.response
-    loader.context.decodeAudioData(
-      request.response,
-      function(buffer) {
-        if (!buffer) {
-          alert('error decoding file data: ' + url);
-          return;
-        }
-        loader.bufferList[index] = buffer;
-        if (++loader.loadCount == loader.urlList.length)
-          loader.onload(loader.bufferList);
-      },
-      function(error) {
-        console.error('decodeAudioData error', error);
-      }
-    );
-  }
+	request.onload = function() {
+	// Asynchronously decode the audio file data in request.response
+		loader.context.decodeAudioData(
+			request.response,
+			function(buffer) {
+			if (!buffer) {
+				alert('error decoding file data: ' + url);
+				return;
+			}
+			loader.bufferList[index] = buffer;
+			if (++loader.loadCount == loader.urlList.length)
+				loader.onload(loader.bufferList);
+			},
+			function(error) {
+				console.error('decodeAudioData error', error);
+			}
+		);
+	}
 
-  request.onerror = function() {
-    alert('BufferLoader: XHR error');
-  }
+	request.onerror = function() {
+		alert('BufferLoader: XHR error');
+	}
 
-  request.send();
+	request.send();
 }
 
 BufferLoader.prototype.load = function() {
-  for (var i = 0; i < this.urlList.length; ++i)
-  this.loadBuffer(this.urlList[i], i);
+	for (var i = 0; i < this.urlList.length; ++i)
+	this.loadBuffer(this.urlList[i], i);
 }
 
 //****************************** end of music functions ******************************
@@ -148,22 +144,20 @@ window.onkeydown = function(e){
 //scene
 var scene = new THREE.Scene();
 
-
 //cameras
 //this is the follow camera
 var cameraA = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 	
-	var camerasr = 0;
-
+var camerasr = 0;
 
 //this is top view camera
 var cameraB = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 		
-		cameraB.position.x = 0;
-		cameraB.position.z = 0;
-		cameraB.position.y = 75;
+cameraB.position.x = 0;
+cameraB.position.z = 0;
+cameraB.position.y = 75;
 
-	var center = new THREE.Vector3( 0, 0,0) ;
+var center = new THREE.Vector3( 0, 0,0) ;
 
 //gemometry
 //this is the look at cube
@@ -171,24 +165,24 @@ var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshPhongMaterial( { color: 0xdddddd, opacity: .5 , transparent: true, specular: 0x009900, shininess: 30, shading: THREE.FlatShading } );
 var cube = new THREE.Mesh( geometry, material );
 		
-		// this the radius for the cube this determins the distance from the center
-		var cubesr = 0;
-		
-		scene.add( cube );
+// this the radius for the cube this determins the distance from the center
+var cubesr = 0;
+
+scene.add( cube );
 
 //this the holder arrays for made geomtry
 var Pointer = [];
 var Madegeomtry = [];
 
 //Spiral geometrt
-  var steps =  200 ;
-  var Stheta = 0;
-  var spiralr = 0;
-  var Spositionx, Spositiony, Spositionz;
-  // array for spiral points
-  var points = [];
+var steps =  200 ;
+var Stheta = 0;
+var spiralr = 0;
+var Spositionx, Spositiony, Spositionz;
+// array for spiral points
+var points = [];
 
-  var spiral = new THREE.SplineCurve3( points );
+var spiral = new THREE.SplineCurve3( points );
   
 	// for loop to genrate the spiral points
 for (var i = 0; i < steps; i++) {
@@ -201,72 +195,63 @@ for (var i = 0; i < steps; i++) {
 };
 
 //spiral lines
-  var lmaterial = new THREE.LineBasicMaterial( { color: 0xffffff, opacity: 1, linewidth: 3, vertexColors: THREE.VertexColors } );
-
-  var Sgeometry = new THREE.Geometry();
-		
-		Sgeometry.vertices = spiral.getPoints( 200 );
+var lmaterial = new THREE.LineBasicMaterial( { color: 0xffffff, opacity: 1, linewidth: 3, vertexColors: THREE.VertexColors } );
+var Sgeometry = new THREE.Geometry();	
+Sgeometry.vertices = spiral.getPoints( 200 );
 
 // Create the final Object3d to add to the scene
-	var splineObject = new THREE.Line( Sgeometry, lmaterial );
-		
-		scene.add( splineObject );
-
-
+var splineObject = new THREE.Line( Sgeometry, lmaterial );
+scene.add( splineObject );
 
 // Lights
 //Inner Array of lights	
-	var lightsAR = 0;
-	var lightsAT = 0;
-	var lightA = 4;
- 	var LightsA = [];
+var lightsAR = 0;
+var lightsAT = 0;
+var lightA = 4;
+var LightsA = [];
 
 // forloop to make lights 	
- 	for (var i = 0; i < lightA; i++) {
- 		var randColors = new THREE.Color(Math.random(),Math.random(),Math.random());
-
- 		var light = new THREE.PointLight( randColors , 1 );
- 			light.position.y= 10; 
- 			LightsA.push(light);
- 			scene.add( light );
+for (var i = 0; i < lightA; i++) {
+	var randColors = new THREE.Color(Math.random(),Math.random(),Math.random());
+	var light = new THREE.PointLight( randColors , 1 );
+	light.position.y= 10; 
+	LightsA.push(light);
+	scene.add( light );
 };
 
 //outer array of lights
 
-	var lightsBR = 0;
-	var lightsBT = 0;
-	var lightB = 4;
- 	var LightsB = [];
+var lightsBR = 0;
+var lightsBT = 0;
+var lightB = 4;
+var LightsB = [];
 
 // forloop to make lights 	
- 	for (var i = 0; i < lightB; i++) {
- 		var randColors = new THREE.Color(Math.random(),Math.random(),Math.random());
-
- 		var light = new THREE.PointLight( randColors , 1 );
- 			light.position.y= 10; 
- 			LightsB.push(light);
- 			//scene.add( light );
+for (var i = 0; i < lightB; i++) {
+	var randColors = new THREE.Color(Math.random(),Math.random(),Math.random());
+	var light = new THREE.PointLight( randColors , 1 );
+	light.position.y= 10; 
+	LightsB.push(light);
+	//scene.add( light );
 };
 
 //-- particles
 var particlesQty = 3000;
+particlesGeometry = new THREE.SphereGeometry( );
+particlesGeometry = new THREE.SphereGeometry(2000, 50, 50);
 
-	particlesGeometry = new THREE.SphereGeometry( );
+materialOptions = {
+    size: 1.0,
+    transparency: true, 
+    opacity: 0.5
+};
 
-    particlesGeometry = new THREE.SphereGeometry(2000, 50, 50);
-
-    materialOptions = {
-        size: 1.0,
-        transparency: true, 
-        opacity: 0.5
-    };
-
-    particlesStuff = new THREE.PointCloudMaterial(materialOptions);
-
+particlesStuff = new THREE.PointCloudMaterial(materialOptions);
 
 for (var i = 0; i < particlesQty; i++) {   
 
 	var particlesVertex = new THREE.Vector3();
+
     particlesVertex.x = Math.random() * 2000 - 1000;
     particlesVertex.y = Math.random() * 1000 - 500;
     particlesVertex.z = Math.random() * 1000 - 500;
@@ -308,7 +293,7 @@ renderer.domElement.addEventListener( 'click', onclick, false );
 
 // add THREE Mouse Camera Orbit Controls to the scene
 var controls = new THREE.OrbitControls(cameraB, renderer.domElement);
-
+controls.maxDistance = 50;
 
 
 function render() {
@@ -318,62 +303,57 @@ function render() {
 
 	if(soundLoaded) analyser.getByteTimeDomainData(dataArray);
 
-//inner light position and movement
+	//inner light position and movement
 	lightsAR += .0125; 
     lightsAT += .0125*Math.PI;
 
     for (var i = 0; i < LightsA.length; i++) {
 		
 		if( i == 0 ){
-		LightsA[i].position.x = lightsAR * Math.sin(lightsAT );
-		LightsA[i].position.z = lightsAR * Math.cos(lightsAT );
+			LightsA[i].position.x = lightsAR * Math.sin(lightsAT );
+			LightsA[i].position.z = lightsAR * Math.cos(lightsAT );
 		}
 
 		else if( i == 1 ){
-		LightsA[i].position.x = lightsAR * Math.sin(lightsAT + (Math.PI/2) );
-		LightsA[i].position.z = lightsAR * Math.cos(lightsAT + (Math.PI/2) );
+			LightsA[i].position.x = lightsAR * Math.sin(lightsAT + (Math.PI/2) );
+			LightsA[i].position.z = lightsAR * Math.cos(lightsAT + (Math.PI/2) );
 		}
 
 		else if( i == 2 ){
-		LightsA[i].position.x = lightsAR * Math.sin(lightsAT + (Math.PI) );
-		LightsA[i].position.z = lightsAR * Math.cos(lightsAT + (Math.PI) );
+			LightsA[i].position.x = lightsAR * Math.sin(lightsAT + (Math.PI) );
+			LightsA[i].position.z = lightsAR * Math.cos(lightsAT + (Math.PI) );
 		}
 
 
 		else{
-		LightsA[i].position.x = lightsAR * Math.sin(lightsAT + ((3*Math.PI) / 2) );
-		LightsA[i].position.z = lightsAR * Math.cos(lightsAT + ((3*Math.PI) / 2) );
+			LightsA[i].position.x = lightsAR * Math.sin(lightsAT + ((3*Math.PI) / 2) );
+			LightsA[i].position.z = lightsAR * Math.cos(lightsAT + ((3*Math.PI) / 2) );
 		}
 	};	
 
-
 	if (limit <= 50 ) {
 
+		theta += .025;
+		cubesr += .025;
+		camerasr = cubesr + 10;
 
-	theta += .025;
-	cubesr += .025;
-	camerasr = cubesr + 10;
-	
+		cube.position.x = cubesr * Math.sin(theta);
+		cube.position.z = cubesr * Math.cos(theta);
+		cameraA.position.x = cubesr * Math.sin(theta + .2 );
+		cameraA.position.z = cubesr * Math.cos(theta + .2 );
 
-
-	cube.position.x = cubesr * Math.sin(theta);
-	cube.position.z = cubesr * Math.cos(theta);
-	cameraA.position.x = cubesr * Math.sin(theta + .2 );
-	cameraA.position.z = cubesr * Math.cos(theta + .2 );
-
-	cameraA.position.y = .5;
-	cameraA.lookAt(cube.position);
-	renderer.render( scene, cameraA );	
+		cameraA.position.y = .5;
+		cameraA.lookAt(cube.position);
+		renderer.render( scene, cameraA );	
 		
 	}
 
 	else{
+
 		theta += .025;
 		cubesr += .025;
 		cube.position.x = cubesr * Math.sin(theta);
 		cube.position.z = cubesr * Math.cos(theta);
-
-		
 		
 		controls.update();
 		
@@ -395,124 +375,18 @@ render();
 
 function Addgeometry(){
 	genFourShapes(Math.floor((Math.random() * 6) + 1));
-	//genFourShapes(5);
-	//for (var i = 0; i < stuff.length; i++) {
-		//var mandala_geometry = new THREE.IcosahedronGeometry( mandala_radius, mandala_detail );
-		// var randY = Math.floor((Math.random() * 10)) - 5 ;
-// 		var randColor = new THREE.Color(Math.random(),Math.random(),Math.random());
-// 		var mandala_geometry = new THREE.DodecahedronGeometry( Math.floor((Math.random() * 2)), Math.floor((Math.random() * 3)) );
-// 	    var mandala_material = new THREE.MeshPhongMaterial( { color: randColor, specular: 0x009900, shininess: 30, shading: THREE.FlatShading } );
-// 	    var cubes = new THREE.Mesh( mandala_geometry, mandala_material );
-// 			cubes.position.x = cube.position.x; 
-// 			cubes.position.y = cube.position.y + randY; 
-// 			cubes.position.z = cube.position.z;
-// 		
-// 		var cubesb = new THREE.Mesh( mandala_geometry, mandala_material );
-// 			cubesb.position.x = -cube.position.x; 
-// 			cubesb.position.y = cube.position.y + randY; 
-// 			cubesb.position.z = -cube.position.z;
-// 
-// 		var cubesc = new THREE.Mesh( mandala_geometry, mandala_material );
-// 			cubesc.position.x = -cube.position.x; 
-// 			cubesc.position.y = cube.position.y + randY; 
-// 			cubesc.position.z = cube.position.z;
-// 
-// 		var cubesd = new THREE.Mesh( mandala_geometry, mandala_material );
-// 			cubesd.position.x =  cube.position.x;  
-// 			cubesd.position.y = cube.position.y + randY; 
-// 			cubesd.position.z = -cube.position.z;
-
-// 		scene.add(cubes);
-
-		// var loader = new THREE.ColladaLoader();
-// 		
-// 		loader.load( 'models/drop.dae', function( collada ){
-// 		
-// 		collada.scene.traverse( function ( child ){
-// 			if ( child instanceof THREE.Mesh ){
-// 				child.material.color = randColor;
-// 				child.material.shininess = 100;
-// 			}
-// 		});
-// 		
-// 		collada.scene.position.x = cube.position.x;			
-// 		collada.scene.position.y = cube.position.y + randY; 
-// 		collada.scene.position.z = cube.position.z;
-// 		
-// 		scene.add(collada.scene);
-// 		});
-// 		
-// 		var loader = new THREE.ColladaLoader();
-// 		loader.load( 'models/drop.dae', function( collada ){
-// 		
-// 		collada.scene.traverse( function ( child ){
-// 			if ( child instanceof THREE.Mesh ){
-// 
-// 				child.material.color = randColor;
-// 				child.material.shininess = 100;
-// 			}
-// 		});
-// 		
-// 		collada.scene.position.x = -cube.position.x;			
-// 		collada.scene.position.y = cube.position.y + randY; 
-// 		collada.scene.position.z = -cube.position.z;
-// 		
-// 		scene.add(collada.scene);
-// 		});
-// 		
-// 		var loader = new THREE.ColladaLoader();
-// 		loader.load( 'models/drop.dae', function( collada ){
-// 		
-// 		collada.scene.traverse( function ( child ){
-// 			if ( child instanceof THREE.Mesh ){
-// 
-// 				child.material.color = randColor;
-// 				child.material.shininess = 100;
-// 			}
-// 		});
-// 		
-// 		collada.scene.position.x = -cube.position.x;			
-// 		collada.scene.position.y = cube.position.y + randY; 
-// 		collada.scene.position.z = cube.position.z;
-// 		
-// 		scene.add(collada.scene);
-// 		});
-// 		
-// 		var loader = new THREE.ColladaLoader();
-// 		loader.load( 'models/drop.dae', function( collada ){
-// 		
-// 		collada.scene.traverse( function ( child ){
-// 			if ( child instanceof THREE.Mesh ){
-// 
-// 				child.material.color = randColor;
-// 				child.material.shininess = 100;
-// 			}
-// 		});
-// 		
-// 		collada.scene.position.x = cube.position.x;			
-// 		collada.scene.position.y = cube.position.y + randY; 
-// 		collada.scene.position.z = -cube.position.z;
-// 		
-// 		scene.add(collada.scene);
-// 		});
-
-
-		// dae.position.x = cube.position.x; 
-// 		dae.position.y = cube.position.y + randY; 
-// 		dae.position.z = cube.position.z;
-// 		scene.add( dae );
 }
 
 function onclick( event ) {
 
-				event.preventDefault();
+	event.preventDefault();
 
-				var ax = cube.position.x;
-				var ay = cube.position.y;
-				var az = cube.position.z;
+	var ax = cube.position.x;
+	var ay = cube.position.y;
+	var az = cube.position.z;
 
-				Pointer.push( new THREE.Vector3( ax, ay, az) );
-				Addgeometry();
+	Pointer.push( new THREE.Vector3( ax, ay, az) );
+	Addgeometry();
 
 };
 
@@ -908,11 +782,9 @@ function genFourShapes(shape){
 		scene.add(cubes);
 		
 	break;
-
 	
 	}
 	
-	console.log(cube.position.x);
 }
 
 
